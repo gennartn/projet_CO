a59aba3791b76917f3e4e81cc8bf8b4baf19a852
Test Nicolas <nicolas.gennart@hotmail.com>
2019-07-03 14:35:20 +0200

    début de projet

---
+++ app/src/main/java/com/example/myapplication/Liste_De_Souhait.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Liste_De_Souhait extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste__de__souhait);
+    }
+}
